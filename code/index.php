3f11ed31427d2ee6c9174b347dcaa613202d74f7
Ekaterina <solomennicova555@gmail.com>
2022-10-30 12:20:19 +0200

    task 2

--- code/index.php
+++ code/index.php
@@ -1,5 +1,18 @@
 <?php
+echo "1 task". "</br>";
 $very_bad_unclear_name  = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= " and 5 chicken legs";
-echo $very_bad_unclear_name;
+echo $very_bad_unclear_name . "</br>";
+
+echo "2 task". "</br>";
+$var1 = 5;
+echo $var1 .  "</br>";
+$var2 = 15.5;
+echo  $var2 . "</br>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month . "</br>";
+
+echo "3 task". "</br>";
+
