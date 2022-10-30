2cf5dfa64cd19acce1433b5ba9d56df20979dac5
Ekaterina <solomennicova555@gmail.com>
2022-10-30 13:02:49 +0200

    task 6

--- code/index.php
+++ code/index.php
@@ -1,28 +1,28 @@
 <?php
-echo "1 task". "</br>";
+echo "1 task". "\n";
 $very_bad_unclear_name  = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= " and 5 chicken legs";
-echo $very_bad_unclear_name . "</br>";
+echo $very_bad_unclear_name . "\n";
 
-echo "</br>" . "2 task". "</br>";
+echo "\n" . "2 task". "\n";
 $var1 = 5;
-echo $var1 .  "</br>";
+echo $var1 .  "\n";
 $var2 = 15.5;
-echo  $var2 . "</br>";
+echo  $var2 . "\n";
 $last_month = 1187.23;
 $this_month = 1089.98;
-echo $last_month - $this_month . "</br>";
+echo $last_month - $this_month . "<\n";
 
-echo "</br>" . "3 task". "</br>";
+echo "\n" . "3 task". "\n";
 $num_languages = 4;
 $mounths = 11;
 $days = $mounths * 16;
 $days_per_language = $days / $num_languages;
-echo "Мэг изучала каждый язык за " . $days_per_language ." дня ". "</br>";
-echo 8**2 . "</br>";
+echo "Мэг изучала каждый язык за " . $days_per_language ." дня ". "\n";
+echo 8**2 . "\n";
 
-echo "</br>" . "4 task". "</br>";
+echo "\n" . "4 task". "\n";
 $my_num = 17;
 $answer = $my_num;
 $answer += 2;
@@ -30,12 +30,12 @@ $answer *= 2;
 $answer -= 2;
 $answer /= 2;
 $answer -= $my_num;
-echo $answer . "</br>";
+echo $answer . "\n";
 
-echo "</br>" . "5 task". "</br>";
+echo "\n" . "5 task". "\n";
 $a = 10;
 $b = 3;
-echo $a%$b . "</br>";
+echo $a%$b . "\n";
 
 if($a/$b == 0){
     echo "Делится " . $a/$b;
@@ -43,7 +43,7 @@ if($a/$b == 0){
 else{
     echo "Делится c остатком " . $a%$b;
 }
-echo "</br>";
+echo "\n";
 $st = pow(2,10);
 $sq = sqrt(245);
 $array = array(4, 2,5 ,19 ,13 ,0, 10);
@@ -51,7 +51,9 @@ $answer = 0;
 foreach ($array as $item){
     $answer += pow($item,2);
 }
-echo sqrt($answer). "</br>";
+echo sqrt($answer). "\n";
 $sq = sqrt(587);
 $as_array = array('round'=>round($sq) , 'ceil'=> ceil($sq) , 'floor'=>floor($sq));
-echo "round:".round($sq) ." ceil:". ceil($sq) ." floor:". floor($sq) . "</br>";
+echo "round:".round($sq) ." ceil:". ceil($sq) ." floor:". floor($sq) . "\n";
+
+echo "\n" . "6 task". "\n";
