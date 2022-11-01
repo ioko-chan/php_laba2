b574ac5e6507dd82a71a7947dc5b595c9f58a591
Ekaterina <solomennicova555@gmail.com>
2022-11-01 15:55:15 +0200

    task 11

--- code/index.php
+++ code/index.php
@@ -9,3 +9,20 @@ function printStr(string $str , int $size){
 
 printStr("-myr-",20);
 
+echo "\n" . "11 task". "\n";
+$array = array(3,4,5,6,7,4,5,5,293,344,4,5,6,7);
+$medium = array_sum($array)/count($array);
+echo $medium. "\n";
+$sum = array_sum(range(1,100));
+$func = function(int $value): float {
+    return sqrt($value )   ;
+};
+$sq = array_map($func,$array);
+echo  print_r($sq). "\n";
+$array_en = range("a","z");
+$array_el = range(1,26);
+$array_en_el = array_combine($array_en,$array_el);
+echo  print_r($array_en_el);
+$str = '1234567890';
+$result = array_sum(str_split($str ,2 ));
+
